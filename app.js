bea16c691b3ff1981943f532bef28a931c65f456
burakJs <burakimdat13@gmail.com>
2022-12-04 05:35:48 +0300

    feat: add models and mongoose config

--- app.js
+++ app.js
@@ -1,14 +1,12 @@
 require('dotenv/config');
-const cors = require('cors');
-const bodyParser = require('body-parser');
-const morgan = require('morgan');
+
 const express = require('express');
 const app = express();
 
-app.use(cors());
-app.use(morgan('dev'));
-app.use(bodyParser.urlencoded({ extended: false }));
-app.use(bodyParser.json());
+const managers = require('./api/managers');
+
+managers.mongooseManager.init();
+managers.appManager.init(app);
 
 app.get('/', (req, res) => {
   return res.json({
