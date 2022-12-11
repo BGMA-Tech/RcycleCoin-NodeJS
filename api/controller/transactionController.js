8d11249478ff9f9d5d7dcc989cb2ad374f2468ed
burakJs <burakimdat13@gmail.com>
2022-12-11 17:40:41 +0300

    feat: add transaction controller

---
+++ api/controller/transactionController.js
@@ -0,0 +1,110 @@
+const mongoose = require('mongoose');
+
+const Transaction = require('../models/transaction');
+const Coin = require('../models/coin');
+const Utils = require('../utils/response');
+
+exports.transactionGetAll = (req, res, next) => {
+  Transaction.find()
+    .select('_id fromPersonelId toPersonelId coinAmount createdAt')
+    .exec()
+    .then((docs) => {
+      const response = {
+        count: docs.length,
+        transactions: docs.map((doc) => {
+          return {
+            _id: doc._id,
+            product: doc.product,
+            quantity: doc.quantity,
+          };
+        }),
+      };
+      Utils.successResponse(res, 200, response);
+    })
+    .catch((err) => {
+      Utils.errorResponse(res, 500, err);
+    });
+};
+
+exports.transactionGetAllById = (req, res, next) => {
+  const id = req.params.transactionId;
+  Transaction.findById(id)
+    .select('_id fromPersonelId toPersonelId coinAmount createdAt')
+    .exec()
+    .then((doc) => {
+      if (doc) {
+        Utils.successResponse(res, 200, doc);
+      } else {
+        Utils.errorResponse(res, 404, {
+          message: 'No valid entry found for provided ID',
+        });
+      }
+    })
+    .catch((err) => {
+      Utils.errorResponse(res, 500, err);
+    });
+};
+
+exports.transactionCreate = async (req, res, next) => {
+  const fromCoin = Coin.findOne({ personelId: req.body.fromPersonelId });
+  const toCoin = Coin.findOne({ personelId: req.body.toPersonelId });
+
+  const transaction = new Transaction({
+    _id: new mongoose.Types.ObjectId(),
+    fromPersonelId: req.body.fromPersonelId,
+    toPersonelId: req.body.toPersonelId,
+    coinAmount: req.body.coinAmount,
+    createdAt: Date.now(),
+  });
+
+  await Promise.all([fromCoin, toCoin]).then(async (values) => {
+    const fromCoin = values[0];
+    const toCoin = values[1];
+
+    if (fromCoin.coinAmount < transaction.coinAmount) {
+      return Utils.errorResponse(res, 500, {
+        message: 'Not enough coin to transfer',
+      });
+    } else {
+      fromCoin.coinAmount -= transaction.coinAmount;
+      toCoin.coinAmount += transaction.coinAmount;
+
+      try {
+        await fromCoin.save();
+        await toCoin.save();
+        await transaction.save();
+
+        return Utils.successResponse(res, 201, {
+          message: 'Transaction stored',
+          createdTransaction: {
+            _id: transaction._id,
+            fromPersonelId: transaction.fromPersonelId,
+            toPersonelId: transaction.toPersonelId,
+            coinAmount: transaction.coinAmount,
+            createdAt: transaction.createdAt,
+          },
+        });
+      } catch (error) {
+        return Utils.errorResponse(res, 500, error);
+      }
+    }
+  });
+
+  // transaction
+  //   .save()
+  //   .then((result) => {
+  //     Utils.successResponse(res, 201, {
+  //       message: 'Transaction stored',
+  //       createdTransaction: {
+  //         _id: result._id,
+  //         fromPersonelId: result.fromPersonelId,
+  //         toPersonelId: result.toPersonelId,
+  //         coinAmount: result.coinAmount,
+  //         createdAt: result.createdAt,
+  //       },
+  //     });
+  //   })
+  //   .catch((err) => {
+  //     Utils.errorResponse(res, 500, err);
+  //   });
+};
--- api/models/transaction.js
+++ api/models/transaction.js
@@ -2,16 +2,8 @@ const mongoose = require('mongoose');
 
 const TransactionSchema = new mongoose.Schema({
   _id: mongoose.Schema.Types.ObjectId,
-  fromCoin: {
-    type: mongoose.Schema.Types.ObjectId,
-    ref: 'Coin',
-    required: true,
-  },
-  toCoin: {
-    type: mongoose.Schema.Types.ObjectId,
-    ref: 'Coin',
-    required: true,
-  },
+  fromPersonelId: { type: String, required: true },
+  toPersonelId: { type: String, required: true },
   coinAmount: { type: Number, required: true },
   createdAt: { type: Date, default: Date.now },
 });
