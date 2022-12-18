b4c2880112801eee4298ace80a2a60e40d46d042
burakJs <burakimdat13@gmail.com>
2022-12-18 20:02:54 +0300

    feat: remove image uploda

--- api/routes/infoRoutes.js
+++ api/routes/infoRoutes.js
@@ -7,7 +7,11 @@ const checkAuth = require('../middleware/checkAuth');
 const upload = GetUploadWithConfig();
 
 router.get('/:id', checkAuth, InfoController.infoGetOne);
-router.post('/', checkAuth, upload.single('image'), InfoController.infoCreate);
+router.post(
+  '/',
+  checkAuth,
+  /*upload.single('image'),*/ InfoController.infoCreate
+);
 router.put('/:id', checkAuth, InfoController.infoUpdate);
 
 module.exports = router;
